debfe6e2761b09ff1772014edd2eb13ee5d75ec8
jianmin22 <jianmin.22@ichat.sp.edu.sg>
2023-10-25 17:53:18 +0800

    did ntg

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -17,9 +17,9 @@ export const postRouter = createTRPCRouter({
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.userTask.create({
+      return ctx.db.post.create({
         data: {
-          title: input.name,
+          name: input.name,
         },
       });
     }),
@@ -27,21 +27,6 @@ export const postRouter = createTRPCRouter({
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
       orderBy: { createdAt: "desc" },
-    });
-  }),
-
-  createTask: publicProcedure
-  .input(z.object({ title: z.string().min(1) ,userId: z.string().min(1)}))
-  .mutation(async ({ ctx, input }) => {
-    // simulate a slow db call
-    await new Promise((resolve) => setTimeout(resolve, 1000));  
-
-    return ctx.db.userTask.create({
-      data: {
-        title: input.title,
-        userId: input.userId,
-        status:true,
-      },
     });
   }),
 });
