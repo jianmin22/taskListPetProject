9e60b297fccc83b4267470593e1144627a14cfbf
jianmin22 <jianmin.22@ichat.sp.edu.sg>
2023-10-25 21:40:40 +0800

    feat: able to get all tasks

---
+++ src/pages/component/showUserTasks.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { api } from "~/utils/api";
+import { useRouter } from "next/router";
+
+interface UserTask {
+  taskID: string;
+  title: string;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  status: boolean;
+}
+
+interface TaskList {
+  listID: string;
+  taskID: string;
+  taskDetails: string;
+  sequence: number;
+  status: boolean;
+}
+
+interface UserTaskWithTaskLists {
+  userTask: UserTask;
+  taskLists: TaskList[];
+}
+
+const ShowUserTasks: React.FC<{ userId: string }> = ({ userId }) => {
+  const { data, isLoading: dataLoading } = api.post.showUserTasks.useQuery({
+    userId: userId,
+  });
+
+  if (dataLoading) return <div className="flex grow">load</div>;
+
+  if (!data) return <div>Error Loading Page</div>;
+
+  return (
+<div className="flex-col justify-center items-center">
+      <div className="text-center">
+        <h2 className="py-4 text-4xl font-bold">Task List</h2>
+      </div>
+
+      {data.length > 0 ? (
+        data.map((tasks) => (
+          <div className="max-w-md rounded-lg bg-white p-6 shadow-lg m-3">
+            <div key={tasks.userTask.taskID} className="mb-4">
+              <div className="mb-2 flex items-center">
+                <h2 className="font-bold text-2xl text-gray-800">
+                  Title:{tasks.userTask.title}
+                </h2>
+              </div>
+              <div>
+                {tasks.taskLists.map((taskList) => (
+                  <div key={taskList.listID} className="mb-2">
+                    <input
+                      type="checkbox"
+                      checked={taskList.status} // Make ticks unclickable
+                      disabled
+                      className="mr-2 cursor-not-allowed"
+                    />
+                    <span className="select-none text-gray-500">
+                      {taskList.taskDetails} -{" "}
+                      <a href={`#`}>View Full Version</a>
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <a href={`#`}>Click to see more details</a>
+          </div>
+        ))
+      ) : (
+        <p>No tasks available.</p>
+      )}
+      
+    </div>
+  );
+};
+
+export default ShowUserTasks;
--- src/server/api/routers/tasks.ts
+++ src/server/api/routers/tasks.ts
@@ -1,6 +1,27 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+interface UserTask {
+  taskID: string;
+  title: string;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  status: boolean;
+}
+
+interface TaskList {
+  listID: string;
+  taskID: string;
+  taskDetails: string;
+  sequence: number;
+  status: boolean;
+}
+
+interface UserTaskWithTaskLists {
+  userTask: UserTask;
+  taskLists: TaskList[];
+}
 
 export const tasksRouter = createTRPCRouter({
   hello: publicProcedure
@@ -11,18 +32,48 @@ export const tasksRouter = createTRPCRouter({
       };
     }),
 
-  createTask: publicProcedure
-  .input(z.object({ title: z.string().min(1) ,userId: z.string().min(1)}))
-  .mutation(async ({ ctx, input }) => {
-    // simulate a slow db call
-    await new Promise((resolve) => setTimeout(resolve, 1000));  
+    showUserTasks: publicProcedure
+    .input(z.object({ userId: z.string().min(1) }))
+    .query(async ({ ctx, input }) => {
+      const userTasks = await ctx.db.userTask.findMany({
+        where: {
+          userId: input.userId,
+        }
+      });
 
-    return ctx.db.userTask.create({
-      data: {
-        title: input.title,
-        userId: input.userId,
-        status:true,
-      },
-    });
-  }),
+      const results: UserTaskWithTaskLists[] = [];
+
+      for (const userTask of userTasks) {
+        const taskLists = await ctx.db.taskList.findMany({
+          where: {
+            taskID: userTask.taskID,
+          },
+          orderBy: {
+            sequence: 'asc',
+          },
+          take: 3,
+        });
+        results.push({
+          userTask,
+          taskLists,
+        });
+      }
+
+      return results;
+    }),
+
+  createTask: publicProcedure
+    .input(z.object({ title: z.string().min(1), userId: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      // simulate a slow db call
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      return ctx.db.userTask.create({
+        data: {
+          title: input.title,
+          userId: input.userId,
+          status: true,
+        },
+      });
+    }),
 });
