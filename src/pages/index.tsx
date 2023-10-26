35bbe4ea5062d5579767422f6b9fae4cad581950
jianmin22 <jianmin.22@ichat.sp.edu.sg>
2023-10-26 14:29:51 +0800

    delete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,7 +69,6 @@ export default function Home() {
   <button className="fixed right-10 bottom-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={openPopup}>
     +
   </button>
-  <CustomPopup isOpen={isPopupOpen} onClose={closePopup} onConfirm={handleConfirm} />
   <ShowUserTasks userId={user.id} />
 </main>
     </div>
